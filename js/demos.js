725a932c06361e0f9cb3d6199867ece3c85b61d1
mrodriguez-litebox <81979614+mrodriguez-litebox@users.noreply.github.com>
2022-06-09 01:05:54 -0300

    Add mergefflocal demo

--- js/demos.js
+++ js/demos.js
@@ -20,8 +20,65 @@ define([], function () {
     originData: [{ id: "e137e9b", tags: ["master"], message: "first commit" }],
   };
 
-  return [
-    free,
-    freeWithRemote,
-  ];
+  var mergefflocal = {
+    title: "Fast-forward merge",
+    key: "mergefflocal",
+    message: "Fast-forward merge",
+    commitData: [
+      {
+        id: "e137e9b",
+        tags: ["origin/master"],
+        message: "first commit",
+        parent: "initial",
+        cx: 50,
+        cy: 379,
+        branchless: false,
+      },
+      {
+        id: "fdc3c45",
+        tags: ["master"],
+        parent: "e137e9b",
+        cx: 140,
+        cy: 379,
+        branchless: false,
+      },
+      {
+        id: "a639b1b",
+        tags: [],
+        parent: "fdc3c45",
+        cx: 230,
+        cy: 379,
+        branchless: false,
+      },
+      {
+        id: "025efab",
+        tags: [],
+        parent: "a639b1b",
+        cx: 320,
+        cy: 379,
+        branchless: false,
+      },
+      {
+        id: "cd4b56d",
+        tags: ["test", "HEAD"],
+        parent: "025efab",
+        cx: 410,
+        cy: 379,
+        branchless: false,
+      },
+    ],
+    originData: [
+      {
+        id: "e137e9b",
+        tags: ["master", "HEAD"],
+        message: "first commit",
+        parent: "initial",
+        cx: 50,
+        cy: 360,
+        branchless: false,
+      },
+    ],
+  };
+
+  return [free, freeWithRemote, mergefflocal];
 });
